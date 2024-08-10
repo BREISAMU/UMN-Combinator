507f898c66af94bc014ada4e01b2a296836b223c
Samuel Breider <samueljbreider@gmail.com>
2024-08-10 10:02:26 -0500

    fix routing

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <code className="font-mono font-bold">umn combinator</code>
+        <Link href="/login">Go to login Page</Link>
       </div>
     </main>
   );
