070e5f2f57f702c25dca7f531b0710cd24141c13
Samuel Breider <samueljbreider@gmail.com>
2024-08-11 11:02:55 -0500

    local

--- api/dbs.py
+++ api/dbs.py
@@ -1,9 +1,9 @@
 from supabase import create_client, Client
 import os
 
-print("\n\nTHIS IS A MESSAGE\n\n")
+print("\n\nTHIS IS A MESSAGE")
 url: str = os.environ.get('UMN_COMBO_URL')
 key: str = os.environ.get('UMN_COMBO_DB_KEY')
 print(url)
-print(key)
+print(str(key) + "\n\n")
 supa_db: Client = create_client(url, key)
